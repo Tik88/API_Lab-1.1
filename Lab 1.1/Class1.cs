1bb2468d29698cb3e2f92a20f75bd81d806ddcf9
Tik88 <xtikx@yandex.ru>
2022-02-09 14:37:21 +0300

    Добавьте файлы проекта.

---
+++ Lab 1.1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace Lab_1._1
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CopuGroup : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберите группу обьектов");
+            Element element = doc.GetElement(reference);
+            Group group = element as Group;
+
+            XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+
+            Transaction transaction = new Transaction(doc);
+            transaction.Start("Копирование группы объектов");
+            doc.Create.PlaceGroup(point, group.GroupType);
+            transaction.Commit();
+
+            return Result.Succeeded;
+
+        }
+    }
+}
